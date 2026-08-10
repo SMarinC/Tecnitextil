f8b6d3604c1f22ab9b91411729c896759816cc2c
Santiago Marin Cardona <marincardona.santiago@hotmail.com>
2026-08-09 19:00:04 -0500

    feat: assemble full landing page from all sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import Header from './components/Header/Header.jsx'
+import Hero from './components/Hero/Hero.jsx'
+import Services from './components/Services/Services.jsx'
+import ValueProps from './components/ValueProps/ValueProps.jsx'
+import HowItWorks from './components/HowItWorks/HowItWorks.jsx'
+import FinalCta from './components/FinalCta/FinalCta.jsx'
+import Footer from './components/Footer/Footer.jsx'
+import WhatsAppFloatingButton from './components/WhatsAppFloatingButton/WhatsAppFloatingButton.jsx'
+
 function App() {
   return (
-    <main>
-      <p>TECNITEXTIL</p>
-    </main>
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <Services />
+        <ValueProps />
+        <HowItWorks />
+        <FinalCta />
+      </main>
+      <Footer />
+      <WhatsAppFloatingButton />
+    </>
   )
 }
 
